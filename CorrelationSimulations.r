23f0862f0e2c61779a4231352cf2e8b48a3bc588
devinpbrown96 <devinpbrown96@gmail.com>
2020-10-09 16:59:58 +0300

    Parallelized and added Graphs

--- CorrelationSimulations.r
+++ CorrelationSimulations.r
@@ -1,4 +1,4 @@
-
+library(doParallel)
 #simulating a nx10 matrix using different PDFs
 matrixcreator <- function(n = 500){
   x1 = rnorm(n, mean= 4, sd = 4)
@@ -28,34 +28,37 @@ ycreator(matrixcreator(1000))
 #creating n.samps matrices and y values, then checking coverage probabilities as well as false positives and false negatives
 sampler = function(n.samps, n = 1000, seed = 1453){
   set.seed(seed)
-  ints = matrix(ncol = 10, nrow = n.samps)
-  falneg = matrix(ncol=3, nrow = n.samps)
-  for (i in 1:n.samps){ 
+  ints = matrix(ncol = 10)
+  falneg = matrix(ncol=3)
+  results_list <- mclapply(1:n.samps, function(x){ 
     X = matrixcreator(n)
     y = ycreator(X)
     mod = lm(y ~ X)
     j = confint(mod)
     in_interval = c(j[2,1]<0.5 & 0.5<j[2,2], j[3,1]<0.2 & 0.2<j[3,2], j[4,1] < -0.8 & -0.8 < j[4,2], apply(j[5:11,], 1, function(x) prod(x) < 0))
-    ints[i,] = in_interval
-    falneg[i,] = apply(j[2:4,], 1, function(x) prod(x) < 0)
+    falneg = apply(j[2:4,], 1, function(x) prod(x) < 0)
+    return(list(in_interval, falneg))
+  }, mc.cores = 4)
+  for (l in results_list){
+    ints = rbind(ints, l[[1]])
+    falneg = rbind(falneg, l[[2]])
   }
-  confint_perc = apply(ints, 2, function(x) sum(x)/n.samps)
-  false_positives = 1- confint_perc[4:10]
-  false_negatives = apply(falneg, 2, function(x) sum(x)/n.samps)
   
+  confint_perc = apply(ints, 2, function(x) mean(x, na.rm = T))
+  false_positives = 1- confint_perc[4:10]
+  false_negatives = apply(falneg, 2, function(x) mean(x, na.rm = T))
   return(list('confint_perc' = confint_perc, 'false_positives' = false_positives, 'false_negatives' = false_negatives))
 }
 
-sampler(500, 10000, 400)
+sampler(1000, 2000, 400)
 
 #changing the number of samples and considering how coverage percentage, false negatives, and false positives change as we increase the number of samples
 varying_samps <- function(runs, n=500, seed = 500){
-  set.seed(seed)
   CI = matrix(nrow= length(runs), ncol = 10)
   FP = matrix(nrow= length(runs), ncol = 7)
   FN = matrix(nrow = length(runs), ncol = 3)
   for (i in 1:length(runs)){
-    iteration = sampler(runs[i], n)
+    iteration = sampler(runs[i], n, seed= 12*i)
     CI[i, ] = iteration$confint_perc
     FP[i, ] = iteration$false_positives
     FN[i, ] = iteration$false_negatives
@@ -63,27 +66,32 @@ varying_samps <- function(runs, n=500, seed = 500){
   return(list('CI' = CI, 'FP' = FP, 'FN' = FN))
 }
 
-runs = seq(50, 2050, 200)
+runs = seq(50, 2050, 100)
 varying_samps(runs)
 
 #creating a correlation between x1 and x4 then checking how coverage changes
 correlationcreator <- function(rho, n.samps=500, n= 2000, seed=600){
   set.seed(seed)
-  ints = matrix(ncol = 10, nrow = n.samps)
-  falneg = matrix(ncol=3, nrow = n.samps)
-  for (i in 1:n.samps){ 
+  ints = matrix(ncol=10)
+  falneg = matrix(ncol=3)
+  results_list <- mclapply(1:n.samps, function(x){ 
     X = matrixcreator(n)  
     X[,4] = rho*X[,1] + sqrt(1 - rho^2)*rnorm(n)
     y = ycreator(X)
     mod = lm(y ~ X)
     j = confint(mod)
     in_interval = c(j[2,1]<0.5 & 0.5<j[2,2], j[3,1]<0.2 & 0.2<j[3,2], j[4,1] < -0.8 & -0.8 < j[4,2], apply(j[5:11,], 1, function(x) prod(x) < 0))
-    ints[i,] = in_interval
-    falneg[i,] = apply(j[2:4,], 1, function(x) prod(x) < 0)
+    falneg = apply(j[2:4,], 1, function(x) prod(x) < 0)
+    return(list(in_interval, falneg))
+  }, mc.cores = 4)
+  for (l in results_list){
+    ints = rbind(ints, l[[1]])
+    falneg = rbind(falneg, l[[2]])
   }
-  confint_perc = apply(ints, 2, function(x) sum(x)/n.samps)
+  
+  confint_perc = apply(ints, 2, function(x) mean(x, na.rm = T))
   false_positives = 1- confint_perc[4:10]
-  false_negatives = apply(falneg, 2, function(x) sum(x)/n.samps)
+  false_negatives = apply(falneg, 2, function(x) mean(x, na.rm = T))
   
   return(list('confint_perc' = confint_perc, 'false_positives' = false_positives, 'false_negatives' = false_negatives))
 }
@@ -93,17 +101,18 @@ correlationcreator(0.3)
 #seeing how coverage changes at different levels of correlation
 varying_rho <- function(rho_sequence, n.samps=500, n =1000, seed = 100){
   set.seed(seed)
-  CI = matrix(nrow= length(rho_sequence), ncol = 10)
-  FP = matrix(nrow= length(rho_sequence), ncol = 7)
-  FN = matrix(nrow = length(rho_sequence), ncol = 3)
+  CI = matrix(ncol = 10, nrow = length(rho_sequence))
+  FP = matrix(ncol = 7, nrow = length(rho_sequence))
+  FN = matrix(ncol = 3, nrow = length(rho_sequence))
   for (i in 1:length(rho_sequence)){
-    iteration = correlationcreator(rho_sequence[i], n.samps, n)
+    iteration = correlationcreator(rho_sequence[i], n.samps, n, seed = 500*i)
     CI[i, ] = iteration$confint_perc
     FP[i, ] = iteration$false_positives
     FN[i, ] = iteration$false_negatives
   }
   return(list('CI' = CI, 'FP' = FP, 'FN' = FN))
 }
+
 
 rho_sequence = seq(0, 0.9, 0.1)
 rho_sequence
@@ -113,26 +122,28 @@ varying_rho(rho_sequence)
 runs = seq(50, 4050, 200)
 CI_plot = varying_samps(runs)
 library(viridis)
-col = viridis(3)
+col = viridis(4)
+setwd('~/Dropbox/QuantMeth')
+pdf('varying_samps_plot.pdf')
 par(mfrow=c(1,1), mgp=c(1.5,0,0), tcl=0, mar=c(3,3,1,1), cex.lab=1, cex.axis=1)
 plot(runs, CI_plot$CI[,1], type= 'l', xlab = "Number of Samples", ylab = "Confidence Estimate", col = col[1], ylim = c(min(CI_plot$CI[,1:3]) - 0.003, max(CI_plot$CI[,1:3]) + 0.003))
-for (i in 2:3) points(runs, CI_plot$CI[,i], col = col[i], type= 'l', lty = i)
-abline(h=0.950, lty = 2)
-legend('topright', legend = c('x1', 'x2', 'x3'), col = col, lty = 1:3 , cex = 0.8, bty = 'n')
-pdf('varying_samps_plot.pdf')
+for (i in 2:3) points(runs, CI_plot$CI[,i], col = col[i], type= 'l', lty = i, lwd = 1.5)
+abline(h=0.950, lty=2)
+legend('topright', legend = c('x1', 'x2', 'x3'), col = col[1:3], lty = 1:3 , cex = 0.8, bty = 'n')
 dev.off()
 
-rho_sequence = seq(0, 0.9, 0.05)
-rho_plot = varying_rho(rho_sequence, 1000, 2000)
+#warning, even parallelized, this rho_plot takes quite some time to run
+rho_sequence = seq(0, 0.95, 0.05)
+rho_plot = varying_rho(rho_sequence, 7000, 3000)
+pdf('varying_rhos_plot1.pdf')
 plot(rho_sequence, rho_plot$CI[,1], type = 'l', xlab = 'Rho Values', ylab = 'X1 Confidence Estimates')
 abline(h = 0.95, lty = 2)
-pdf('varying_rhos_plot1.pdf')
 dev.off()
 
 par(mfrow=c(1,1), mgp=c(1.5,0,0), tcl=1, mar=c(3,3,1,1), cex.lab=1, cex.axis=1)
+pdf('varying_rhos_plot2.pdf')
 plot(rho_sequence, rho_plot$FP[,1], type = 'l', xlab = 'Rho Values', ylab = 'X4 False Positive Rate')
 abline(h = 0.95, lty = 2)
-pdf('varying_rhos_plot2.pdf')
 dev.off()
 
 
